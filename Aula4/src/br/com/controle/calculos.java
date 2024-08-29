a36918de9a4087e44532abff34e719c8b8055d1c
Gabriel Novaes <80960360+Yukitte@users.noreply.github.com>
2024-08-29 11:12:46 -0300

    Add files via upload

---
+++ Aula4/src/br/com/controle/calculos.java
@@ -0,0 +1,16 @@
+
+package br.com.controle;
+
+public class calculos {
+    
+    private double valor;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor*2;
+    }
+    
+}
