b73d85089b77bb0ecdda022f53be9b9837d278ca
Gabriel Novaes <80960360+Yukitte@users.noreply.github.com>
2024-09-19 11:11:57 -0300

    Add files via upload

---
+++ Polimorfismo/src/controle/Media.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controle;
+
+/**
+ *
+ * @author gabriel.snovaes
+ */
+public class Media extends Operação{
+    public Double calcular(Double valor1, Double valor2){
+            return (valor1 + valor2)/2;
+        }
+    
+}
---
+++ Polimorfismo/src/controle/Operação.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controle;
+
+/**
+ *
+ * @author gabriel.snovaes
+ */
+public class Operação {
+    public Double calcular(Double valor1, Double valor2){
+            return 0.0;
+        }
+}
