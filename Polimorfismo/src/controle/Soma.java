e15cec2296a7050529894b73aadd3ed1d3dacee4
Gabriel Novaes <80960360+Yukitte@users.noreply.github.com>
2024-09-19 10:34:17 -0300

    Add files via upload

---
+++ Polimorfismo/src/controle/Soma.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controle;
+
+public class Soma extends Operação {
+        public Double calcular(Double valor1, Double valor2){
+            return valor1 + valor2;
+        }
+}
